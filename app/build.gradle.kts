275bcf06cac062ff7bc00469620a549962e606a6
Subrat-4050 <sahusubrat83184@gmail.com>
2025-05-22 20:28:38 +0530

    Update prerequisites

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.topbar"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.topbar"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
